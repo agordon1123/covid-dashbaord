026afbc24b687a25a680524c4c1376edc86d6116
Alex <agordon1123@gmail.com>
2020-07-07 23:21:23 -0400

    connected api and created percentage fill - wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import MapChart from './components/MapChart';
+import axios from 'axios';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({});
+  const url = 'https://api.covid19api.com/summary';
+
+  useEffect(() => {
+    axios.get(url)
+      .then((res) => {
+        let dict = {};
+        let sortedData = res.data.Countries.sort((a, b) => b.TotalConfirmed - a.TotalConfirmed);
+        let largest = sortedData[0].TotalConfirmed;
+        dict['largest'] = largest;
+        sortedData.map(el=> dict[el.CountryCode] = el.TotalConfirmed);
+        setData(dict);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
   return (
     <div className="App">
-      <h1>Covid-19 Dashbaord</h1>
-      <MapChart />
+      <h1>Covid-19 Dashboard</h1>
+      {Object.keys(data).length ? (
+        <MapChart data={data} />
+      ) : null}
     </div>
   );
 }
--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,13 +1,36 @@
 import React, { useState } from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
-import * as data from '../data/ne_110m_countries_copy.json';
+import * as mapData from '../data/ne_110m_countries_copy.json';
 
-const MapChart = () => {
+const MapChart = (props) => {
+    const { data } = props;
+
+    const calcColor = (percentage) => {
+        let r, g, b = 0;
+        if (percentage < 50) {
+            r = 255;
+            g = Math.round(5.1 * percentage);
+        } else {
+            g = 255;
+            r = Math.round(510 - 5.1 * percentage);
+        }
+        var h = r * 0x10000 + g * 0x100 + b * 0x1;
+        return '#' + ('000000' + h.toString(16)).slice(-6);
+    }
+    
     return (
         <div>
             <ComposableMap>
-                <Geographies geography={data.default}>
-                    {({ geographies }) => geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)}
+                <Geographies geography={mapData.default}>
+                    {({ geographies }) => 
+                        geographies.map(geo => {
+                            let totalCases = data[geo.properties.ISO_A2];
+                            let percentage = Math.round(data[geo.properties.ISO_A2] / data['largest']) * 100;
+                            return (
+                                <Geography key={geo.rsmKey} geography={geo} fill={totalCases !== undefined ? calcColor(percentage) : null} />
+                            )
+                        })
+                    }
                 </Geographies>
             </ComposableMap>
         </div>
